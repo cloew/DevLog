1e7c8e80c52975d3bf85869d81e9fff58b619e9d
Chris Loew <cloew123@gmail.com>
2015-09-30 11:41:37 -0600

    Updated config to store its values via TOML

--- devlog/config/config.py
+++ devlog/config/config.py
@@ -1,9 +1,12 @@
 from kao_config import GlobalConfigFile
 from kao_decorators import lazy_property
 
+import toml
+
 class Config:
     """ Represents the DevLog Configuration """
     CONFIG_FILENAME = ".devlog"
+    EDITOR_KEY = 'editor'
     
     @lazy_property
     def config(self):
@@ -13,10 +16,19 @@ class Config:
     @property
     def editorCommandString(self):
         """ Return the configured editor command String """
-        return self.config.readlines()[0].strip()
+        return self._toml[self.EDITOR_KEY]
         
     def setEditor(self, editorCmd):
         """ Set the editor command """
-        self.config.write(editorCmd.strip())
+        self._write(editor=editorCmd.strip())
+        
+    @property
+    def _toml(self):
+        """ Return the toml dictionary from the file """
+        return toml.loads(self.config.read())
+        
+    def _write(self, **kwargs):
+        """ Write the configuration with the data given """
+        self.config.write(toml.dumps(kwargs))
         
 Config = Config()
