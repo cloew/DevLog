ecc2643826b277206cbec8e8a1b91f958cdbcfb9
Chris Loew <cloew123@gmail.com>
2015-09-30 21:49:18 -0600

    Now use the Kao Path

--- devlog/file_structure.py
+++ devlog/file_structure.py
@@ -1,16 +1,6 @@
 from .config import Config
+from kao_path import TouchDirectory, TouchFile
 import os
-
-def CreateDirectoryIfItDoesNotExist(directory):
-    """ Creates the given directory if it does not exist """
-    if not os.path.isdir(directory):
-        os.mkdir(directory)
-
-def CreateFileIfItDoesNotExist(filename):
-    """ Creates the given file if it does not exist """
-    if not os.path.exists(filename):
-        with open(filename, 'w'):
-            pass
             
 def create_path(createFn):
     """ Decorator to create a path from the path provided """
@@ -23,8 +13,8 @@ def create_path(createFn):
         return wrapper
     return decorator
             
-create_dir = create_path(CreateDirectoryIfItDoesNotExist)
-create_file = create_path(CreateFileIfItDoesNotExist)
+create_dir = create_path(TouchDirectory)
+create_file = create_path(TouchFile)
 
 class FileStructure:
     """ Helper class to manage the File Structure for the dev log """
