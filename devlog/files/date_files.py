9fda0da40e6cf1c91ad18ab65c65a6493d7a2010
Chris Loew <cloew123@gmail.com>
2015-10-02 11:16:24 -0600

    Added proeprty to specify the Date Field for the Date Files

--- devlog/files/date_files.py
+++ devlog/files/date_files.py
@@ -13,14 +13,14 @@ class DateFiles(Enum):
     
     def __call__(self, date=None):
         if date is None:
-            if self.value is DateFields.year:
+            if self.field is DateFields.year:
                 date = datetime.date.today()
             else:
                 raise TypeError('Missing 1 required argument: date')
         return [self.build(date, dateValue) for dateValue in self.getDirContents(self.path(date))]
         
     def path(self, date, create=False):
-        return PathBuilder().getPath(date, self.value, create=False)
+        return PathBuilder().getPath(date, self.field, create=False)
         
     def build(self, date, value):
         return DateBuilder(self.date).build({self.field, value})
@@ -28,3 +28,8 @@ class DateFiles(Enum):
     def getDirContents(self, directory):
         """ Return the contents of the given directory """
         return sorted([int(file) for file in os.listdir(directory)])
+        
+    @property
+    def field(self):
+        """ Return the Date Field for this set of Date Files """
+        return self.value
