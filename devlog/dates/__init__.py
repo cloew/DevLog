289df575b02c93bd7c7165f9ec300f97e36c61b0
Chris Loew <cloew123@gmail.com>
2015-10-02 17:04:52 -0600

    Added Next and Previous properties to a log file

---
+++ devlog/dates/__init__.py
@@ -0,0 +1,3 @@
+from .log_day import LogDay
+from .log_month import LogMonth
+from .log_year import LogYear
--- devlog/log_file.py
+++ devlog/log_file.py
@@ -1,3 +1,4 @@
+from .dates import LogDay, LogMonth, LogYear
 from .files import DateFiles
 from kao_decorators import lazy_property
 
@@ -13,3 +14,24 @@ class LogFile:
     def path(self):
         """ Return the path to this log file """
         return DateFiles.Days.path(self.date, create=self.create)
+    
+    @property
+    def previous(self):
+        """ Return the previous LogFile """
+        previous = self.day.previous
+        return LogFile(previous.date)
+    
+    @property
+    def next(self):
+        """ Return the next LogFile """
+        next = self.day.next
+        return LogFile(next.date)
+        
+    @property
+    def day(self):
+        """ Return the LogDay """
+        path = self.path # Force file creation if needed
+        year = LogYear(date)
+        month = LogMonth(date, year)
+        return LogDay(date, month)
+        
