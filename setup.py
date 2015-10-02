9b52f28d5a7bde30d79560fa63bc6da40132e1f1
Chris Loew <cloew123@gmail.com>
2015-10-02 17:18:50 -0600

    Added proper install script

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from distutils.core import setup
 
 setup(name='devlog',
-      version='0.0.1',
+      version='0.1.0',
       description="",
-      author='',
-      author_email='',
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
       packages=['devlog',
                 'devlog.commands',
-                'devlog.config'],
+                'devlog.config',
+                'devlog.dates',
+                'devlog.files'],
+      scripts=['devlog/scripts/devlog']
      )
